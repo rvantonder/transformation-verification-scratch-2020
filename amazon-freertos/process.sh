fcee9ea04e4396102bffbec5e63cb2992c9135fb
Rijnard van Tonder <rvantonder@gmail.com>
2020-12-14 13:01:42 -0700

    update

--- amazon-freertos/process.sh
+++ amazon-freertos/process.sh
@@ -1,1 +1,1 @@
-comby 'Doing :[name.] :[_] Time vanilla: real :[t1.] :[_] Time comby: real :[t2.]' ':[name],:[t1],:[t2]' raw-output.txt -newline-separated -stdout
+comby 'Doing :[name.] :[_] Time vanilla: real :[t1.]s :[_] Time comby: real 0m:[t2.]s' ':[name],:[t1],:[t2]' raw-output.txt -newline-separated -stdout > output.txt 
